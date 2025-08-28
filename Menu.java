02c1b3e6beffc1a0873a7fec9cf2b3ccd188642c
Matheus-Leffa <matheusleffa@live.com>
2025-08-27 22:59:45 -0300

    versão final

--- Menu.java
+++ Menu.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        while (true){
+        boolean continuar = true;
+        
+        while (continuar) {
             System.out.println("""
                     Calculadora Simples:
                     Digite o primeiro número:""");
@@ -18,20 +20,32 @@ public class Menu {
                     2 - Subtração
                     3 - Multiplicação
                     4 - Divisão
+                    5 - Finalizar programa
                     Digite aqui:""");
             int action = teclado.nextInt();
-            switch (action){
+            switch (action) {
                 case 1:
-                Double resultSoma = operacoes.somar(n1, n2);
+                    double resultSoma = operacoes.somar(n1, n2);
                     System.out.println("A soma entre " + n1 + " e " + n2 + " é " + resultSoma);
                     break;
                 case 2:
-                    Double resultSub = operacoes.subtrair(n1, n2);
+                    double resultSub = operacoes.subtrair(n1, n2);
                     System.out.println("A subtração entre " + n1 + " e " + n2 + " é " + resultSub);
                     break;
                 case 3:
-                    Double resultMulti = operacoes.multiplicar(n1, n2);
+                    double resultMulti = operacoes.multiplicar(n1, n2);
                     System.out.println("A multiplicação entre " + n1 + " e " + n2 + " é " + resultMulti);
+                    break;
+                case 4:
+                    String resultDiv = operacoes.dividir(n1, n2);
+                    System.out.println(resultDiv);
+                    break;
+                case 5:
+                    System.out.println("Finalizando o programa...");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Digite um valor válido!");
                     break;
             }
         }
--- operacoes.java
+++ operacoes.java
@@ -10,4 +10,11 @@ public class operacoes {
     public static double multiplicar(double num1, double num2){
         return num1 * num2;
     }
-}
+    public static String dividir(double num1, double num2) {
+        if (num2 != 0) {
+            return "A divisão entre " + num1 + " e " + num2 + " é " + num1/num2;
+        } else {
+            return "Não é possível divisão por 0!";
+        }
+    }
+    }
