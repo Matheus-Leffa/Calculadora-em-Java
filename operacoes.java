00946b99ca1d74cee7638a32b55bfeda96cfd3df
Matheus-Leffa <matheusleffa@live.com>
2025-08-27 22:16:45 -0300

    first commit

--- operacoes.java
+++ operacoes.java
@@ -4,7 +4,4 @@ public class operacoes {
     public static double somar(double num1, double num2){
         return num1 + num2;
     }
-    public static double subtrair(double num1, double num2){
-        return num1 - num2;
-    }
 }
