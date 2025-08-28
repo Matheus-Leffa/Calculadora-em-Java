1b2dccd8d37b7fbe9d15860126d2cb7d55f9c185
Matheus-Leffa <matheusleffa@live.com>
2025-08-27 21:44:20 -0300

    first commit

---
+++ Menu.java
@@ -0,0 +1,31 @@
+package Calculadora;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        while (true){
+            System.out.println("""
+                    Calculadora Simples:
+                    Digite o primeiro número:""");
+            double n1 = teclado.nextDouble();
+            System.out.println("Digite o segundo número: ");
+            double n2 = teclado.nextDouble();
+            System.out.println("""
+                    Escolha qual operação deseja fazer:
+                    1 - Adição
+                    2 - Subtração
+                    3 - Multiplicação
+                    4 - Divisão
+                    Digite aqui:""");
+            int action = teclado.nextInt();
+            switch (action){
+                case 1:
+                Double resultSoma = operacoes.somar(n1, n2);
+                    System.out.println("A soma entre " + n1 + " e " + n2 + " é " + resultSoma);
+                    break;
+            }
+        }
+    }
+}
---
+++ operacoes.java
@@ -0,0 +1,7 @@
+package Calculadora;
+
+public class operacoes {
+    public static double somar(double num1, double num2){
+        return num1 + num2;
+    }
+}
