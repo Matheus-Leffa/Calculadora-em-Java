63b5887849f36f417705c3587a6a51a9bda1cdaf
Matheus-Leffa <matheusleffa@live.com>
2025-08-27 22:10:10 -0300

    implementada função subtrair

--- operacoes.java
+++ operacoes.java
@@ -4,4 +4,7 @@ public class operacoes {
     public static double somar(double num1, double num2){
         return num1 + num2;
     }
+    public static double subtrair(double num1, double num2){
+        return num1 - num2;
+    }
 }
